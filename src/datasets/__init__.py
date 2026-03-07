8c216545d1719562f81db50a5b2a59c355cbbe4c
Raviksan Navaratnarajah <raviksann@outlook.com>
2026-03-07 15:51:31 +0100

    slurm job added

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -1,3 +1,4 @@
 from .brats_h5 import BraTSH5Dataset
+from .brats_transforms import ConvertToMultiChannelBasedOnBratsClassesd
 
-__all__ = ["BraTSH5Dataset"]
+__all__ = ["BraTSH5Dataset", "ConvertToMultiChannelBasedOnBratsClassesd"]
